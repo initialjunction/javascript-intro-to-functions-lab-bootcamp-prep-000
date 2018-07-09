98f2f5f6234cd20a64510b3298a5fa4208037dc6
Joanna Tomassoni <jctomassoni@gmail.com>
2018-07-09 20:06:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,5 +27,6 @@ function sayHiToGrandma(string)
    return "I can\'t hear you!"
  } else if (uppercase.toUpperCase(string) === uppercase) {
    return "YES INDEED!"
- } else (mixedCase.toLowerCase(string)) !=== mixedCase)
+ } else (mixedCase.toLowerCase(string)) === mixedCase)
+ {
 }
