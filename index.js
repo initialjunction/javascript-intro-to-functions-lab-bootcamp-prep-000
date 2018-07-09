767bc9f94c827fbba812e19dd6712efe649f2446
Joanna Tomassoni <jctomassoni@gmail.com>
2018-07-09 20:15:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,5 +27,8 @@ function sayHiToGrandma(string)
    return "I can\'t hear you!"
  } else if (uppercase.toUpperCase(string) === uppercase) {
    return "YES INDEED!"
+ } else if string === "I love you, Grandma."
+ {
+   return "I love you, too."
  }
 }
